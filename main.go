a4eab1d71f79dac5f02df8e792253c36583db756
이근석(Geunseok.Lee) <gs.lee@cndfactory.com>
2020-03-27 17:17:19 +0900

    안쓰는 코드 제거

--- main.go
+++ main.go
@@ -5,25 +5,12 @@ import (
 	"net/http"
 	"strconv"
 
-	"cndf.order.was/model"
 	"cndf.order.was/route"
 	"github.com/labstack/echo"
 )
 
-// 기본 포트 81
+// PORT 기본 포트는 81
 const PORT = 81
-
-var messageId int64 = 0
-
-// createMessage 더미 메시지 생성
-func createMessage(message string, sender string) model.Message {
-	messageId++
-	return model.Message{
-		ID:      messageId,
-		Sender:  sender,
-		Message: message,
-	}
-}
 
 // main 프로그램의 시작점
 func main() {
@@ -40,17 +27,4 @@ func main() {
 	e.PUT("/user", route.UserPut)
 	e.DELETE("/user/:id", route.UserDelete)
 	e.Logger.Fatal(e.Start(":" + strconv.Itoa(PORT)))
-
-	// // 특정 url주소로 Request가 들어올 경우 처리하는 handler함수를 할당 HandleRequest
-	// http.HandleFunc("/message", httphandlers.HandleRequest)
-
-	// // http WAS 서비스 온!
-	// var err = http.ListenAndServe(":"+strconv.Itoa(PORT), nil)
-
-	// // 로그 작성 후 main 함수는 끗 이후에는 http를 통해 request가 오면 callback 하는 보통의 WAS와 동일
-	// if err != nil {
-	// 	log.Panicln("Server failed starting. Error: ", err)
-	// } else {
-	// 	log.Panicln("Server starting. At ", PORT, " Port")
-	// }
 }
