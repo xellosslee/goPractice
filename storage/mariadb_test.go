d185153484e7ae72be434fa20c912fd90bf91c7a
이근석(Geunseok.Lee) <gs.lee@cndfactory.com>
2020-06-17 16:53:37 +0900

    자료형 변경

--- storage/mariadb_test.go
+++ storage/mariadb_test.go
@@ -50,7 +50,7 @@ func TestConnectDB(t *testing.T) {
 	log.Println("insertId ", insertId)
 
 	// 방금 추가한 레코드 한건 PK 값으로 조회
-	var id int
+	var id int64
 	var name, login_id string
 	row := storage.SelectOne(db, "SELECT id, name, login_id FROM users WHERE id = ?", insertId)
 	err = row.Scan(&id, &name, &login_id)
